c18e4c49e3ea72b6c37647da442f5120cddd1373
Aurora <aurora.grefsrud@gmail.com>
2023-01-12 10:24:26 +0100

    documentation and clean up

--- src/root_to_2dhists.py
+++ src/root_to_2dhists.py
@@ -8,11 +8,25 @@ import sys
 
 from imcal import *
 
+"""
+This script takes in 3-5 arguments (input root file location, where to save the new file, filename, resolution and number of events) and
+outputs a .hdf5 file and a log file. The root file must be made using Delphes, and contain calorimeter and track information. The script 
+can be changed to work with different root files.
+The created hdf5 file has the two datasets, "image" and "labels" which can be used for machine learning purpose.
+Each image has the resolution set in this script, and the label given by the filename. The functions used in the script can be found in
+the imcal.py script in the same folder as this script.
+
+Example usage:
+cd src
+python ./root_to_2dfhists.py -f "/my/data/rootfiles/higgs.root" -s "/my/data/histograms/" -n "higgs" -r 50 -N 1000
+
+"""
+
 ##Parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', type=str, required=True, help="File to process. Must be .root")
 parser.add_argument('-s', '--savepath', type=str, required=True, help="Where to save .hd5 output.")
-parser.add_argument('-n', '--name', type=str, default = "empty", help="Name of file to be created.")
+parser.add_argument('-n', '--name', type=str, default = "empty", help="Name of file to be created (without extension). Also the label of the dataset.")
 parser.add_argument('-r', '--resolution', type=int, default = 80, 
                         help="Resolution of histograms. If not specified, it will be 80. Must be an integer*10.")
 parser.add_argument('-N', type=int, default = -1, 
@@ -28,48 +42,45 @@ logging.info(f"Reading file {args.file}")
 MAX_EVENTS = args.N #images to make
 savepath = args.savepath
 filename = args.name
-data_paths = [f"{args.file}:Delphes"] #Is in list format because of too lazy to change
+data_path = f"{args.file}:Delphes" 
 
 #Set resolution
 MIN_RES = 20
 if args.resolution%10==0:
     RESOLUTION = args.resolution
 else: 
-    logging.info("Resolution must be dividable ny 10.")
+    logging.info("Resolution must be dividable by 10.")
     logging.critical("Invalid resolution format.")
     sys.exit(1)
 
 #Load data
-clusters = [load_data(path, MAX_EVENTS, "Tower", 
+clusters = load_data(data_path, MAX_EVENTS, "Tower", 
                         ["Tower.ET", "Tower.Eta", "Tower.Phi", "Tower.Eem", "Tower.Ehad", "Tower.E"])
-                        for path in data_paths]
-tracks = [load_data(path, MAX_EVENTS, "Track", 
+tracks = load_data(data_path, MAX_EVENTS, "Track", 
                         ["Track.PT", "Track.Eta", "Track.Phi"])
-                        for path in data_paths]
-MAX_EVENTS = len(clusters[0])
+MAX_EVENTS = len(clusters)
 
-logging.info(f"Number of events loaded: {[len(item) for item in clusters]}")
+logging.info(f"Number of events loaded: {len(clusters)}")
 
-#Pad Tower data and normalise
-max_hits = np.max([np.max([len(event) for event in item["Eta"]]) for item in clusters])
+#Pad Tower data
+max_hits = np.max([len(event) for event in clusters["Eta"]])
 logging.info(f"Padding towers to size: {max_hits}")
-clusters = [ak.pad_none(item, max_hits, axis=-1) for item in clusters]
+clusters = ak.pad_none(clusters, max_hits, axis=-1)
 
-#Pad track data and pad none
-max_hits = np.max([np.max([len(event) for event in item["Eta"]]) for item in tracks])
+#Pad track data
+max_hits = np.max([len(event) for event in tracks["Eta"]])
 logging.info(f"Padding tracks to size: {max_hits}")
 tracks = [ak.pad_none(item, max_hits, axis=-1) for item in tracks]
 
 # Creating the histograms
+hists_Eem = create_histograms(ak.to_numpy(clusters.Phi), ak.to_numpy(clusters.Eta), 
+                                ak.to_numpy(clusters.Eem), MAX_EVENTS, RESOLUTION)
+hists_Ehad = create_histograms(ak.to_numpy(clusters.Phi), ak.to_numpy(clusters.Eta), 
+                                ak.to_numpy(clusters.Ehad), MAX_EVENTS, RESOLUTION)
+hists_tracks = create_histograms(ak.to_numpy(tracks.Phi), ak.to_numpy(tracks.Eta), 
+                                    ak.to_numpy(tracks.PT), MAX_EVENTS, RESOLUTION)
 
-hists_Eem = create_histograms(ak.to_numpy(clusters[0].Phi), ak.to_numpy(clusters[0].Eta), 
-                                ak.to_numpy(clusters[0].Eem), MAX_EVENTS, RESOLUTION)
-hists_Ehad = create_histograms(ak.to_numpy(clusters[0].Phi), ak.to_numpy(clusters[0].Eta), 
-                                ak.to_numpy(clusters[0].Ehad), MAX_EVENTS, RESOLUTION)
-hists_tracks = create_histograms(ak.to_numpy(tracks[0].Phi), ak.to_numpy(tracks[0].Eta), 
-                                    ak.to_numpy(tracks[0].PT), MAX_EVENTS, RESOLUTION)
-
-#Stack to RGB
+#Stack to 3 channel
 images = np.stack((hists_Eem, hists_Ehad, hists_tracks), axis=-1)
 logging.info(f"Image data shape: {images.shape}")
 
@@ -77,8 +88,9 @@ logging.info(f"Image data shape: {images.shape}")
 meta = {
     "Resolution": RESOLUTION,
     "Events" : MAX_EVENTS,
-    "Input" : data_paths[0]
+    "Input" : data_path
 }
+
 #Save
 saved = store_hists_hdf5(images, savepath, filename, meta)
 logging.info(f"File saved as: {saved}")
